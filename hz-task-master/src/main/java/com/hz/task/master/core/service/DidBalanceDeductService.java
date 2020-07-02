d3df80284b80cf716e2fd2978f0a9a3a4066477c
df-HelloWorld <duanfeng_1712@qq.com>
2020-07-02 16:10:17 +0800

    实现功能10

---
+++ hz-task-master/src/main/java/com/hz/task/master/core/service/DidBalanceDeductService.java
@@ -0,0 +1,13 @@
+package com.hz.task.master.core.service;
+
+
+import com.hz.task.master.core.common.service.BaseService;
+
+/**
+ * @Description 用户扣减余额流水的Service层
+ * @Author yoko
+ * @Date 2020/6/20 12:06
+ * @Version 1.0
+ */
+public interface DidBalanceDeductService<T> extends BaseService<T> {
+}
